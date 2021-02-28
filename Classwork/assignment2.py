087f2d3824c33cd3cfb930e3feb039b6d2299cdf
Shruti <shrutis1051@gmail.com>
2021-02-28 22:02:08 +0530

    Fixing readability and adding comments

--- Classwork/assignment2.py
+++ Classwork/assignment2.py
@@ -29,11 +29,13 @@ plt.savefig("./plots/a2/org_img_vis.png", dpi=200)
 plt.show()
 
 
+
 print("Dimension/Shape of the image: ", brain_image.shape)
 print("Pixel values: ")
 print("Max pixel value {} by {} pixels.".format(np.max(brain_image), np.count_nonzero(brain_image==np.max(brain_image))))
 print("Min pixel value {} by {} pixels.\n".format(np.min(brain_image), np.count_nonzero(brain_image==np.min(brain_image))))
 # print("Frequency corresponding to unique pixel values: ", Counter(brain_image.ravel()))
+
 
 
 # ========================================================================================================================
@@ -43,27 +45,33 @@ print("Min pixel value {} by {} pixels.\n".format(np.min(brain_image), np.count_
 # Initialize a 16x16 plot
 fig = plt.figure(figsize=(15, 20))
 
+
 # First subplot in the 2x1 figure
 plt.subplot(121)
+
 # Plotting the histogram with a separate bin for all intensity values from 0-255
 plt.hist(brain_image.ravel(), bins=list(range(0, 256)))
 plt.xlabel('Pixel Intensity bins')
 plt.ylabel('Frequency')
 plt.title("Histogram with 256 bins, one bin for each intensity value")
 
+
 # Second subplot in the 2x1 figure
 plt.subplot(122)
+
 # Plotting the histogram with 16 bins to group together high and low intensity pixel values and visualize the frequencies better
 freq, bins, patches = plt.hist(brain_image.ravel(), bins=16, edgecolor='black')
 plt.xlabel('Pixel Intensity bins')
 plt.ylabel('Frequency')
-# Annotate the frequency of each bin
+
+# Annotate the frequency of each bin in the plot
 for _, (fr, be, patch) in enumerate(zip(freq, bins, patches)):
     # To fix overlapping of freq labels
     if _ % 2 == 0:
         plt.annotate("{}".format(int(fr)), xy=(be, int(fr)+1000)) 
     else:
         plt.annotate("{}".format(int(fr)), xy=(be, int(fr)+4000)) 
+
 plt.title("Histogram with 16 bins")
 
 # Add title to the figure
@@ -72,6 +80,7 @@ fig.suptitle("Histogram of the original brain image")
 # Save the figure and display on screen
 plt.savefig('./plots/a2/hist_org_brain.png', dpi=200)
 plt.show()
+
 
 
 # ========================================================================================================================
@@ -85,32 +94,41 @@ for sigma in sigma_values:
 
     # Initialize a 5x5 plot for the smoothed image
     fig = plt.figure(figsize=(5, 5))
+
+    # Plot the gaussian filtered brain image
     plt.imshow(smoothed_image, cmap="Greys_r")
     plt.title(r"Smoothed image with $\sigma$ = {}".format(sigma))
     plt.savefig("./plots/a2/smoothed_img_sigma_{}.png".format(sigma), dpi=200)
     plt.show()
 
+
     # Initialize a 10x5 plot
     fig = plt.figure(figsize=(15, 20))
 
+    # Plot the histogram with 256 bins
     plt.subplot(121)
     freq, bins, patches = plt.hist(smoothed_image.ravel(), bins=list(range(0, 256)))
     plt.xlabel('Pixel Intensity bins')
     plt.ylabel('Frequency')
     plt.title(r"Histogram of smoothed image with $\sigma$ = {} with 256 bins".format(sigma))
 
+
+    # Plot the histogram with 16 bins
     plt.subplot(122)
     freq, bins, patches = plt.hist(smoothed_image.ravel(), bins=16, edgecolor='black')
     plt.xlabel('Pixel Intensity bins')
     plt.ylabel('Frequency')
-    # Annotate the frequency of each bin
+
+    # Annotate the frequency of each bin in the plot
     for _, (fr, be, patch) in enumerate(zip(freq, bins, patches)):
         # To fix overlapping of freq labels
         if _ % 2 == 0:
             plt.annotate("{}".format(int(fr)), xy=(be, int(fr)+1000)) 
         else:
             plt.annotate("{}".format(int(fr)), xy=(be, int(fr)+4000)) 
+
     plt.title(r"Histogram of smoothed image with $\sigma$ = {} with 16 bins".format(sigma))
+
 
     # Title of the figure
     fig.suptitle("Histogram of the gaussian filtered image")
@@ -119,6 +137,8 @@ for sigma in sigma_values:
     plt.savefig("./plots/a2/hist_smoothed_sigma_{}.png".format(sigma), dpi=200)
     plt.show()
 
+
+    # Print the max and min intensity pixel values
     print(r"Pixel values for smoothed image with $\sigma$ = ", sigma)
     print("Max pixel value {} by {} pixels.".format(np.max(smoothed_image), np.count_nonzero(smoothed_image==np.max(smoothed_image))))
     print("Min pixel value {} by {} pixels.\n".format(np.min(smoothed_image), np.count_nonzero(smoothed_image==np.min(smoothed_image))))
