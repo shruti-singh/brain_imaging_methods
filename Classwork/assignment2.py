7fef2f4c18733cf5f4d6e3e241a9ee629836e6a3
Shruti <shrutis1051@gmail.com>
2021-02-28 20:58:21 +0530

    Plot histogram

--- Classwork/assignment2.py
+++ Classwork/assignment2.py
@@ -33,4 +33,35 @@ print("Dimension/Shape of the image: ", brain_image.shape)
 print("Pixel values: ")
 print("Max pixel value {} by {} pixels.".format(np.max(brain_image), np.count_nonzero(brain_image==np.max(brain_image))))
 print("Min pixel value {} by {} pixels.\n".format(np.min(brain_image), np.count_nonzero(brain_image==np.min(brain_image))))
-print("Frequency corresponding to unique pixel values: ", Counter(brain_image.ravel()))
+# print("Frequency corresponding to unique pixel values: ", Counter(brain_image.ravel()))
+
+
+# ========================================================================================================================
+# Plot histogram of the original image
+# ========================================================================================================================
+
+# Initialize a 16x16 plot
+fig = plt.figure(figsize=(16, 16))
+
+# First subplot in the 2x1 figure
+plt.subplot(121)
+# Plotting the histogram with a separate bin for all intensity values from 0-255
+plt.hist(brain_image.ravel(), bins=list(range(0, 256)))
+plt.xlabel('Pixel Intensity bins')
+plt.ylabel('Frequency')
+plt.title("Histogram with 256 bins, one bin for each intensity value")
+
+# Second subplot in the 2x1 figure
+plt.subplot(122)
+# Plotting the histogram with 16 bins to group together high and low intensity pixel values and visualize the frequencies better
+freq, bins, patches = plt.hist(brain_image.ravel(), bins=16, edgecolor='black')
+plt.xlabel('Pixel Intensity bins')
+plt.ylabel('Frequency')
+# Annotate the frequency of each bin
+for fr, be, patch in zip(freq, bins, patches):
+    plt.annotate("{}".format(int(fr)), xy=(be, int(fr)+1000)) 
+plt.title("Histogram with 16 bins")
+
+# Save the figure and display on screen
+plt.savefig('./plots/a2/hist_org_brain.png', dpi=500)
+plt.show()
