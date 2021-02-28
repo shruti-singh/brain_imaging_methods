6810b51565ae5d6914200edc77041219b8a458ac
Shruti <shrutis1051@gmail.com>
2021-02-28 20:55:24 +0530

    Dimension of the array. Max and min pixel intensities.

--- Classwork/assignment2.py
+++ Classwork/assignment2.py
@@ -1,6 +1,8 @@
 # Import statements
 import matplotlib.pyplot as plt
 from scipy import ndimage
+import numpy as np
+from collections import Counter
 
 brain_image = plt.imread('Brain.jpg') 
 
@@ -25,3 +27,10 @@ plt.savefig("./plots/a2/org_img_vis.png", dpi=500)
 
 # Display on screen
 plt.show()
+
+
+print("Dimension/Shape of the image: ", brain_image.shape)
+print("Pixel values: ")
+print("Max pixel value {} by {} pixels.".format(np.max(brain_image), np.count_nonzero(brain_image==np.max(brain_image))))
+print("Min pixel value {} by {} pixels.\n".format(np.min(brain_image), np.count_nonzero(brain_image==np.min(brain_image))))
+print("Frequency corresponding to unique pixel values: ", Counter(brain_image.ravel()))
