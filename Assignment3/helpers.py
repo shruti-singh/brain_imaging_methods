75521d0594732eb402bd35f5b2a42cdbc8613567
Shruti <shrutis1051@gmail.com>
2021-04-08 23:56:41 +0530

    update generation of non words for len<=4 and len>7

--- Assignment3/helpers.py
+++ Assignment3/helpers.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 
 def replace_vowels(word):
     replacement_map = {'a': 'u', 'e': 'i', 'i': 'a', 'o': 'e', 'u': 'o'}
@@ -13,13 +15,36 @@ def replace_vowels(word):
     return word.lower()
 
 
+def random_word_perturbation(word, single_char=False):
+    non_word = ""
+
+    if single_char:
+        # Randomly change only one char in word
+        pos = np.random.randint(0, len(word))
+        char = np.random.randint(97, 123)
+        word[pos] = char
+    else:
+        # Randomly change 1/3 characters of the total word length
+        size = len(word)//3
+        for i in size:
+            pos = np.random.randint(0, len(word))
+            char = np.random.randint(97, 123)
+            word[pos] = char
+    return word
+
+
 def create_en_non_words(word_list):
     "If len of the word is greater than 4 and less than 7, replace vowels with other vowels. Remove vowels. "
 
     non_words = []
 
     for w in word_list:
+        w = w.lower()
         if len(w) > 4 and len(w) < 7:
             non_words.append(replace_vowels(w))
+        elif len(w) <= 4:
+            random_word_perturbation(w, single_char=True)
+        else:
+            random_word_perturbation(w)
 
     return non_words
